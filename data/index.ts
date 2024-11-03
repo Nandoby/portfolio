a7f925cf842178465312dcf4a7c30a1e41764ded
Nando Biaccalli <nandobiaccalli@gmail.com>
2024-11-03 19:54:00 +0100

    testimonials

--- data/index.ts
+++ data/index.ts
@@ -103,3 +103,69 @@ export const projects = [
     link: "/ui.apple.com",
   },
 ];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Ferdinando was an absolute pleasure. His professionalism, promptness, and dedication to deliviring exceptional results were evident throughout our project. Ferdinando's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ferdinando is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Ferdinando was an absolute pleasure. His professionalism, promptness, and dedication to deliviring exceptional results were evident throughout our project. Ferdinando's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ferdinando is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Ferdinando was an absolute pleasure. His professionalism, promptness, and dedication to deliviring exceptional results were evident throughout our project. Ferdinando's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ferdinando is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Ferdinando was an absolute pleasure. His professionalism, promptness, and dedication to deliviring exceptional results were evident throughout our project. Ferdinando's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ferdinando is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Ferdinando was an absolute pleasure. His professionalism, promptness, and dedication to deliviring exceptional results were evident throughout our project. Ferdinando's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Ferdinando is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "Hostinger",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
